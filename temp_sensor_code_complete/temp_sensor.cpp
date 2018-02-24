d88df60f63451836735169f4e4cfa273f44ae327
Andrew Rose <andrew.rose43@gmail.com>
2018-02-24 13:39:19 -0800

    Removed a leftover commented-out code block

--- temp_sensor_code_complete/temp_sensor.cpp
+++ temp_sensor_code_complete/temp_sensor.cpp
@@ -43,14 +43,6 @@ float TempSensor::get_temp(){
 }
 
 //given a pointer to a float, writes its four bytes to serial
-/*
-void TempSensor::write_float(float * temp_ptr){
-  byte * ptr2 = (byte*) temp_ptr + 3;
-  for (int i = 0; i++; i<4){
-    Serial.write(ptr2, 1);
-    ptr2--;
-  }
-  */
 void TempSensor::write_float(float * temp_ptr){
   byte * tt = (byte*) temp_ptr;
   for (int i = 0; i < 4; i++){
