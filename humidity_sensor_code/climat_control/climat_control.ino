3634d400496e5e8ca09d1590f9448dc3fff038a5
Matthew-Buckler <32558872+Matthew-Buckler@users.noreply.github.com>
2018-03-31 08:52:04 -0700

    bug fixes to climat control

--- humidity_sensor_code/climat_control/climat_control.ino
+++ humidity_sensor_code/climat_control/climat_control.ino
@@ -1,14 +1,20 @@
 #include "SparkFun_Si7021_Breakout_Library.h"
 #include <Wire.h>
+#define TCAADDR 0x70
 
-float humidity = 0;
-float tempC = 0;
+float humidity;
+float tempC;
 
 Weather humiditySensor;
 
-const int fanPin = 11;
+const uint8_t fanPin1 = 3;
+const uint8_t fanPin2 = 5;
+const uint8_t fanPin3 = 6;
 
-uint8_t fanSpeed = 0;
+uint8_t fanSpeed1 = 0;
+uint8_t fanSpeed2 = 0;
+uint8_t fanSpeed3 = 0;
+
 
 void setup()
 {
@@ -20,19 +26,34 @@ void setup()
 
 void loop()
 {
-  getWeather();
+  for (int sensornum = 0; sensornum < 8; sensornum++) //pings all sensors attached to i2c expander
+  {
+    tcaselect(sensornum);
 
-  fanSpeed = map(tempC, 10, 30, 0, 255); //10, 30 == sensor value range (that has been converted in celcius)   ||   0, 255 == pwm (fan control) range
-  fanSpeed = constrain(fanSpeed, 0, 255);
-  analogWrite(fanPin, fanSpeed);
+    tempC = humiditySensor.getTemp();//get.Temp == celcius  ||  get.TempF == farenhight
 
-  delay(10);
+    if (sensornum == 1) {
+      fanSpeed1 = map(tempC, 10, 30, 0, 255); //maps fan1 to the port 1 of of i2c expander (sensor1)
+      analogWrite(fanPin1, fanSpeed1);
+    }
+
+    else if (sensornum == 2) {
+      fanSpeed2 = map(tempC, 10, 30, 0, 255); //maps both fan 2 & 3 to the port 2 of the i2c expander (sensor2)
+      analogWrite(fanPin2, fanSpeed2);
+
+      fanSpeed3 = map(tempC, 10, 30, 0, 255);
+      analogWrite(fanPin3, fanSpeed3);
+
+    }
+  }
+  delay(20);//short delay to slow things down
 }
 
 
-void getWeather()
-{
-  humidity = humiditySensor.getRH();
+void tcaselect(uint8_t i) { // (if on port ex. 4, select 4 here)
+  if (i > 7) return;
 
-  tempC = humiditySensor.getTemp();//get.Temp == celcius  ||  get.TempF == farenhight
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
 }
